1017ad890876603568a7856168056cbfb10b6846
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-01 16:56:23 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -6,4 +6,13 @@ class Cat
     @name = name
     @mood = "nervous"
   end
+
+  def owner=(new_owner)
+    if new_owner.is_a? Owner
+         if !(@owner == new_owner)
+           @owner = new_owner
+         end
+       end
+     end
+    
 end
